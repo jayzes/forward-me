8a742075ff3a89e92814ec26fb2a55be61e35e30
Jay Zeschin <jay.zeschin@modeset.com>
2014-04-26 12:36:05 -0600

    Customize the forwarder 'from' display name to be nicer

--- app/mailers/forward_mailer.rb
+++ app/mailers/forward_mailer.rb
@@ -1,7 +1,5 @@
 class ForwardMailer < ActionMailer::Base
 
-  default from: ENV['FORWARDING_FROM_ADDRESS']
-
   def forward_from_incoming(email)
     create_attachments_from_griddler_email(email)
     send_from_griddler_email(email)
@@ -17,7 +15,7 @@ class ForwardMailer < ActionMailer::Base
 
   def send_from_griddler_email(email)
     recipients = forward_recipients_from_griddler_email(email)
-    mail(to: recipients, reply_to: email.from, subject: email.subject) do |format|
+    mail(to: recipients, reply_to: email.from, from: sender_from_griddler_email(email), subject: email.subject) do |format|
       format.text { render text: email.raw_text }
       format.html { render html: email.raw_html.html_safe }
     end
@@ -25,6 +23,10 @@ class ForwardMailer < ActionMailer::Base
 
   def forward_recipients_from_griddler_email(email)
     ForwardingAddress.by_source_emails(*email.to.map { |r| r[:email] }).map(&:recipient_emails).flatten
+  end
+
+  def sender_from_griddler_email(email)
+    "\"#{email.from} (via forwarder)\" <#{ENV['FORWARDING_FROM_ADDRESS']}>"
   end
 
 end
